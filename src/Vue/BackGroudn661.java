c8d3b30bba2d131348b7f2b5f0c6773f8b5e3578
1625545 <1625545@li-h015-0t18>
2018-03-26 11:00:39 -0400

    backGroundssss3, bouton bonne grosseur

--- src/Vue/BackGroudn661.java
+++ src/Vue/BackGroudn661.java
@@ -3,13 +3,14 @@ package Vue;
 import java.awt.Image;
 import java.awt.Toolkit;
 import javax.swing.ImageIcon;
+import javax.swing.JPanel;
 
 
-public class BackGroudn661 extends ImageIcon  {
+public class BackGroudn661 extends JPanel  {
      
 static Image image661 = Toolkit.getDefaultToolkit().getImage("ImagesNiveaux/6.61.png");
     public BackGroudn661() {
-        super(image661);
+        super();
     }
     
 }
