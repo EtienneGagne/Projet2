5e82d973fc15d89b95c111c34d142638247c64db
audre <audre@10.150.134.149>
2018-04-09 08:14:20 -0400

    set bounds des boutons dans backgroudn419, voir ligne 86,87 dans Fenetre 2

--- src/Vue/BackGroudn419.java
+++ src/Vue/BackGroudn419.java
@@ -4,10 +4,12 @@ import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.Toolkit;
 import javax.swing.JPanel;
+import javax.swing.TransferHandler;
 
 public class BackGroudn419 extends JPanel{
 
     Image image419 = Toolkit.getDefaultToolkit().getImage("ImagesNiveaux/4.19.png");
+    BackGroudn419 button[] = new BackGroudn419[3];   
 
     @Override
     public void paintComponent(Graphics g) {
@@ -20,5 +22,17 @@ public class BackGroudn419 extends JPanel{
     public BackGroudn419(int hauteur, int largeur) {
         setSize(largeur, hauteur);
     }
+    
+    public void init() {
+    add(button[0]);
+    add(button[1]);
+    add(button[2]);
+    button[0].setBounds(280,125,100,75);
+    button[0].setTransferHandler(new TransferHandler("icon"));
+    button[1].setBounds(280,125,100,75);
+    button[1].setTransferHandler(new TransferHandler("icon"));
+    button[2].setBounds(280,125,100,75);
+    button[3].setTransferHandler(new TransferHandler("icon"));
+}
 
 }
