3bc3ec7c83a7ba49a80a5dcf29edaeeff758bcd7
1637157 <1637157@li-h015-0t05>
2018-05-07 10:43:00 -0400

    batterie 15v

--- src/ca/qc/bdeb/prog4/circuitAEC/Main.java
+++ src/ca/qc/bdeb/prog4/circuitAEC/Main.java
@@ -1,15 +1,17 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Circuit AEC
  */
 package ca.qc.bdeb.prog4.circuitAEC;
 
 import Vue.Fenetre;
+import javax.swing.UIManager;
+import javax.swing.UIManager.LookAndFeelInfo;
+import javax.swing.UnsupportedLookAndFeelException;
 
 /**
- *
- * @author 1637157
+ * @author Étienne Gagné
+ * @author Audrey Lupien
+ * @author Cloé Lachance
  */
 public class Main {
 
@@ -17,9 +19,25 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-
+       //création fenetre
        Fenetre fenetre=new Fenetre();
-        
+     try {
+    for (LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+        if ("Nimbus".equals(info.getName())) {
+            UIManager.setLookAndFeel(info.getClassName());
+            break;
+        }
+    }
+} catch (UnsupportedLookAndFeelException e) {
+    // handle exception
+} catch (ClassNotFoundException e) {
+    // handle exception
+} catch (InstantiationException e) {
+    // handle exception
+} catch (IllegalAccessException e) {
+    // handle exception
+}
+
     }
     
 }
